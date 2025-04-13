ce19e4fa14c8c4437d9b4d2a88bbeb2784622ea4
nguyenadamq <nguyenadamq@gmail.com>
2025-04-13 02:56:18 -0700

    finished ascii figlet file

--- asciiArt.js
+++ asciiArt.js
@@ -1,4 +1,15 @@
 const { argv } = require('node:process');
+var figlet = require("figlet")
 
+var userString = argv[2]
 // print process.argv
-console.log(argv[2])
+console.log(userString)
+
+figlet(userString, function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
